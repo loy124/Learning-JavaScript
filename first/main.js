1183db127a85b65ae74ab68f07fe2bccd002fe41
이온유 <dhsdb1541@naver.com>
2019-03-16 22:46:15 +0900

    for을 활용해서 반복 구조 만들어보기

--- first/main.js
+++ first/main.js
@@ -1,1 +1,17 @@
-console.log('main.js loaded');
+$(document).ready(function() {
+    'use strict';
+    console.log('main.js loaded');
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+
+    let c;
+    for (let x=25; x <400; x+=50) {
+        for(let y=25; y<400; y+=50 ) {
+            c = Shape.Circle(x, y, 20);
+            c.fillColor = 'green';
+        }
+    }
+
+
+    paper.view.draw();
+});
