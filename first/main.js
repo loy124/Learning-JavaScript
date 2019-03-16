a2560ee6d25a6aa94c24080c62b06fe5b55756f0
이온유 <dhsdb1541@naver.com>
2019-03-16 22:56:33 +0900

    hello world! 기초부터 다시  공부 시작

--- first/main.js
+++ first/main.js
@@ -3,14 +3,21 @@ $(document).ready(function() {
     console.log('main.js loaded');
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
+    
+    const tool = new Tool();
+    tool.onMouseDown = function(event) {
+        const a = Shape.Circle(event.point, 20);
+        a.fillColor = 'green';
+    };
 
-    let c;
-    for (let x=25; x <400; x+=50) {
-        for(let y=25; y<400; y+=50 ) {
-            c = Shape.Circle(x, y, 20);
-            c.fillColor = 'green';
-        }
-    }
+    const c = Shape.Circle(200, 200, 80);
+    c.fillColor = 'black';
+    const text = new PointText(200,200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content = 'hello.world';
+
 
 
     paper.view.draw();
