337f48568f47dcec68c08a1de19086f355e0e559
이온유 <dhsdb1541@naver.com>
2019-03-17 20:45:52 +0900

    2장 gulp및 eslint 적용하기

---
+++ public/dist/dist/test.js
@@ -0,0 +1,26 @@
+'use strict'; //es6 기능 : 블록 스코프 변수 선언
+
+var sentences = [{
+  subject: 'JavaScript',
+  verb: 'is',
+  object: 'great'
+}, {
+  subject: 'Elephants',
+  verb: 'are',
+  object: 'large'
+}]; //es6 기능 : 객체 분해
+
+function say(_ref) {
+  var subject = _ref.subject,
+      verb = _ref.verb,
+      object = _ref.object; //es6기능: 템플릿 문자열
+  //아래 사용한것은 백틱 ``
+
+  console.log("".concat(subject, " ").concat(verb, " ").concat(object));
+} // es6 기능 : for..of
+
+
+for (var _i = 0; _i < sentences.length; _i++) {
+  var s = sentences[_i];
+  say(s);
+}
