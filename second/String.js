20c3be5078c37c481312ed85601566fc8890cfbc
이온유 <dhsdb1541@naver.com>
2019-03-17 22:20:22 +0900

    숫자 프로퍼티

---
+++ second/String.js
@@ -0,0 +1,8 @@
+const small = Number.EPSILON;//1을 더햇을때 1과 구분되는 결과를 만들수 있는 가장 작은 값
+const bigInt = Number.MAX_SAFE_INTEGER; //표현할수있는 가장 큰 정수
+const max = Number.max_value//표현할수 있는 가장 큰 숫자
+const minInt = Number.MIN_SAFE_INTEGER;//표현할 수 있는 가장 작은 정수
+const min = Number.MIN_VALUE; // 표현 할 수 있는 가장 작은 숫자
+const nInf = Number.NEGATIVE_INFINITY; // -무한대
+const nan = Number.NaN;// NAN
+const inf = Number.POSITIVE_INFINITY;// + 무한대
